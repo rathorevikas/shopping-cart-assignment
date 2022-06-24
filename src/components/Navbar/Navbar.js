28fb392bef781a5b271206256cd25022525144ca
Vikas Rathore <vikratho1@publicisgroupe.net>
2022-06-24 11:04:55 +0530

    menu dropdown added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import logo from "../../static/images/logo.png";
 import React, { useState } from "react";
 import "./Navbar.css";
 import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import MenuIcon from '@mui/icons-material/Menu';
 import { useSelector } from "react-redux/es/exports";
 import { Link, useNavigate } from "react-router-dom";
 import Cart from "../Cart/Cart";
@@ -10,16 +11,18 @@ import Cart from "../Cart/Cart";
 
 const Navbar = () => {
   const [renderCart, setRenderCart] = useState(false);
+  const [showDropdown, setShowDropDown] = useState(false);
   const navigate = useNavigate();
 
   const cart = useSelector((state) => state.cart);
 
   const renderCartHandler = (event) => {
-    if((event.type === "click" )|| (event.type === "keypress" && event.key ==="Enter") )
-    {
+    if (
+      event.type === "click" ||
+      (event.type === "keypress" && event.key === "Enter")
+    ) {
       setRenderCart(!renderCart);
     }
-
   };
 
   return (
@@ -35,12 +38,30 @@ const Navbar = () => {
         >
           <Grid item spacing={3} container>
             <Grid item xs={4} sm={3}>
-              <img src={logo} alt="logo" aria-label="logo" onClick={() => navigate("/")} />
+              <img
+                src={logo}
+                alt="logo"
+                aria-label="logo"
+                onClick={() => navigate("/")}
+              />
             </Grid>
             <Grid item xs={4} sm={6} container>
               <Grid item className="navlink_container display_none">
                 <Link to="/">Home</Link>
                 <Link to="/products">Products</Link>
+              </Grid>
+              <Grid item className="loginDropdown">
+                <MenuIcon
+                  onClick={() => setShowDropDown(!showDropdown)}
+                  sx={{ fontSize: 40 , color:"var(--darkpink)", marginTop:"5px" }}
+                />
+                {showDropdown && (
+                  <Grid item className="dropdown-content">
+                    <Link to="/products">Products</Link>
+                    <Link to="/signin">Login</Link>
+                    <Link to="/signup">Register</Link>
+                  </Grid>
+                )}
               </Grid>
             </Grid>
             <Grid item xs={4} sm={3} container direction="column">
@@ -53,16 +74,22 @@ const Navbar = () => {
                   item
                   justifyContent="center"
                   onClick={renderCartHandler}
-                  onKeyPress ={renderCartHandler}
+                  onKeyPress={renderCartHandler}
                   tabIndex={0}
                   container
-                  sx={{width:"fit-content"}}
+                  sx={{ width: "fit-content" }}
                 >
                   <Grid item className="graybackground">
-                    <ShoppingCartRoundedIcon sx={{ color: "var(--darkpink)" }} />
+                    <ShoppingCartRoundedIcon
+                      sx={{ color: "var(--darkpink)" }}
+                    />
                   </Grid>
                   <Grid item className="graybackground">
-                    <Typography aria-label={`${cart.cartTotalQuantity} items in cart`} >{cart.cartTotalQuantity} items</Typography>
+                    <Typography
+                      aria-label={`${cart.cartTotalQuantity} items in cart`}
+                    >
+                      {cart.cartTotalQuantity} items
+                    </Typography>
                   </Grid>
                 </Grid>
               </Grid>
